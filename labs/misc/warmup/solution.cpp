6d92f7f3926a134c78d63d169c79a18195b69f5f
Blake <56631911+BlakeLin-LSY@users.noreply.github.com>
2022-12-09 00:47:06 +0800

    Modify warmup solution

--- labs/misc/warmup/solution.cpp
+++ labs/misc/warmup/solution.cpp
@@ -1,10 +1,23 @@
 
 #include "solution.h"
 
-int solution(int *arr, int N) {
-  int res = 0;
-  for (int i = 0; i < N; i++) {
-    res += arr[i];
+#ifdef SOLUTION
+  // your solution
+  int solution(int *arr, int N) {
+    // int res = 0;
+    // for (int i = 0; i < N; i++) {
+    //   res += arr[i];
+    // }
+    // return res;
+    return ((N+1)*N/2);
   }
-  return res;
-}
+#else
+  // baseline version
+  int solution(int *arr, int N) {
+    int res = 0;
+    for (int i = 0; i < N; i++) {
+      res += arr[i];
+    }
+    return res;
+  }
+#endif
